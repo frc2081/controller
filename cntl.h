52782ffb6d8c2fb426e4339471272d48104780c3
Lunar Dust <infinitycreeper1@gmail.com>
2017-01-09 20:20:27 -0600

    Add more comments

--- cntl.h
+++ cntl.h
@@ -17,16 +17,21 @@ typedef JoystickButton Btn_t;
 class btn
 {
 	private:
+		//JoystickButton object
 		Btn_t *raw;
 
 	public:
+		//The current state of the given button
 		bool State;
+		//True if the current state is true and the last state is false
 		bool RE;
+		//The state of the button on the previous loop
 		bool Hld;
 
 		//Called for all buttons on a controller by calling UpdateCntl()
 		void Update();
 
+		//Button constructor
 		btn(int, Joystick**);
 		virtual ~btn() = default;
 };
@@ -34,16 +39,17 @@ class btn
 class cntl
 {
 	private:
+		//Joystick object
 		Joystick *stick;
 
 	public:
 		//Returns controller object, takes controller number as argument
 		cntl(int);
 
-		//Should be called twice at the beginning of every cycle, once for each controller
+		//Should be called for each controller at the beginning of each teleop loop
 		void UpdateCntl();
 
-
+		//Button objects of the controller
 		btn *bA;
 		btn *bB;
 		btn *bX;
